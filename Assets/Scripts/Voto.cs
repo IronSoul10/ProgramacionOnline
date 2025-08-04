299ea505c2f8842214aa3d2edaa79a952294eb5e
Iron <llaltairll@outlook.com>
2025-08-04 03:18:48 -0600

    TAREA 15

---
+++ Assets/Scripts/Voto.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Voto : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI nickName;
+    [SerializeField] private Transform emptyImage;
+    [SerializeField] private Transform panelVotantes;
+    [SerializeField] private Button selfbutton;
+
+}
